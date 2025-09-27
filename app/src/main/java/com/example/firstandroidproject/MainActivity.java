226191be3d0f54a2c153a9bde44aabb79933e8df
Tran Quang Phuoc <phuocde308@gmail.com>
2025-09-27 20:04:07 +0700

    Khai báo activity trong AndroidManifest cho file ManHinhDangKi java vừa tạo , xử lí sự kiện nhấn nút tạo tài khoản sang trang đăng kí tài khoản

--- app/src/main/java/com/example/firstandroidproject/MainActivity.java
+++ app/src/main/java/com/example/firstandroidproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.firstandroidproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCreateAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Ánh xạ nút "Tạo tài khoản"
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        // Sự kiện bấm nút
+        btnCreateAccount.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ManHinhDangKi.class);
+            startActivity(intent);
         });
     }
 }
---
+++ app/src/main/java/com/example/firstandroidproject/ManHinhDangKi.java
@@ -0,0 +1,12 @@
+package com.example.firstandroidproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManHinhDangKi extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.man_hinh_dang_ki); // layout đăng ký đã tạo
+    }
+}
